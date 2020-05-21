c5100fa52907c6b308b5eeed0607efc52890db0f
Anduin Xue <anduin@aiursoft.com>
2020-05-21 09:55:16 +0800

    Upgrade NiBot API Level.

--- NiBot.Kahla/Models/NiCommand.cs
+++ NiBot.Kahla/Models/NiCommand.cs
@@ -1,19 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Kahla.SDK.Events;
+using System;
 using System.Threading.Tasks;
-using Kahla.SDK.Events;
 
 namespace NiBot.Kahla.Models
 {
-    public delegate Task CommandHandler(string arg, NewMessageEvent eventContext);
-
     public class NiCommand
     {
         public string Cmd { get; set; }
         public string ArgFormat { get; set; }
         public string Disruption { get; set; }
 
-        public CommandHandler Handler { get; set; }
+        public Func<string, NewMessageEvent, Task> Handler { get; set; }
     }
 }
